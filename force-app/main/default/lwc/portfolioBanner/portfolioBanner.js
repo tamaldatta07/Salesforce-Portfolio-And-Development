c9a6920fd36a36383a0c92b0adbf8dc7c55a16a7
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-08-08 17:06:35 +0530

    portfolio banner code is updated.
    User can add links of social sites.
    Logo will be visible only for the added links.
    Links can be changed from community cloud. No need to update code on VS Code.

---
+++ force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
@@ -0,0 +1,46 @@
+import { LightningElement, wire, api} from "lwc";
+import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import FULLNAME from "@salesforce/schema/Portfolio__c.FullName__c";
+import COMPANY_LOCATION from "@salesforce/schema/Portfolio__c.CompanyLocation__c";
+import COMPANY_NAME from "@salesforce/schema/Portfolio__c.CompanyName__c";
+import DESIGNATION from "@salesforce/schema/Portfolio__c.Designation__c";
+
+export default class PortfolioBanner extends LightningElement {
+  @api recordId; //= "a015i000011NJLiAAO";
+  @api linkedinUrl; //= "https://in.linkedin.com/in/tamal-datta-758493241?trk=people-guest_people_search-card";
+  @api githubUrl; //= "https://github.com/tamaldatta07";
+  @api trailheadUrl; //= "https://www.salesforce.com/trailblazer/tdatta11";
+  @api blogUrl;
+  @api youtubeUrl;
+  @api twitterUrl;
+
+
+  userPic = `${PortfolioAssets}/PortfolioAssets/userPic.jpeg`;
+  linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`;
+  youtube = `${PortfolioAssets}/PortfolioAssets/Social/youtube.svg`;
+  github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`;
+  twitter = `${PortfolioAssets}/PortfolioAssets/Social/twitter.svg`;
+  trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`;
+  blog = `${PortfolioAssets}/PortfolioAssets/Social/blogger.svg`;
+
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [FULLNAME, COMPANY_LOCATION, COMPANY_NAME, DESIGNATION],
+  })
+  portfolioData;
+
+  get fullName() {
+    return getFieldValue(this.portfolioData.data, FULLNAME);
+  }
+  get companyLocation() {
+    return getFieldValue(this.portfolioData.data, COMPANY_LOCATION);
+  }
+  get companyName() {
+    return getFieldValue(this.portfolioData.data, COMPANY_NAME);
+  }
+  get designation() {
+    return getFieldValue(this.portfolioData.data, DESIGNATION);
+  }
+
+}
