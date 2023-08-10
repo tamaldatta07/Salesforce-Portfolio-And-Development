8004271f84c6295d7b8864aa4eb8d08e176fcce8
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-08-10 16:26:28 +0530

    Code updated
    User Contact details Component is updated.
    Added button for resume download.

---
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+
+export default class PortfolioUserDetails extends LightningElement {
+    @api recordId
+    @api objectApiName
+
+    downloadResume(){
+
+    }
+}
--- force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
+++ force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {}
+export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {
+    recordId="a015i000011NJLiAAO"
+    objectApiName="Portfolio__c"
+}
