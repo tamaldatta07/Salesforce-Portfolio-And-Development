c30412badab73d36e59bb24c592d04690f8665ef
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-08-14 14:08:16 +0530

    Code Updated
    User Summary details section is updated

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -7,5 +7,5 @@ export default class PortfolioUserDetails extends LightningElement {
     downloadResume(){
         window.open(this.resumeUrl,"_blank")
     }
-    //"https://github.com/karkranikhil/nikhil-resume/raw/main/SFdummyResume%20(1).pdf"
+    
 }
