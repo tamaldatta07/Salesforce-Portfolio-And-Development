de6905fe043e0b74d79fe233720a590d555733e7
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-10-16 20:16:31 +0530

    Logic fix for 12:00PM scenario

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -26,33 +26,38 @@ export default class AlarmClockApp extends LightningElement {
         this.createMinutesOptions()
         this.currentTimeHandler()
     }
-    currentTimeHandler(){
-
-        setInterval(()=>{
-        let dateTime = new Date()
-        let hour = dateTime.getHours()  //getHours returns the hour (0 to 23) of a date
-        let min = dateTime.getMinutes()
-        let sec = dateTime.getSeconds()
-        let ampm = "AM"
-        if(hour == 0){
-            hour = 12
-        } else if(hour >= 12){
-            hour = hour -12
-            ampm = "PM"
-        }
-        hour = hour < 10 ? "0"+hour : hour
-        min = min < 10 ? "0"+min : min
-        sec = sec < 10 ? "0"+sec : sec
-        this.currentTime =`${hour}:${min}:${sec} ${ampm}`
-        if(this.alarmTime === `${hour}:${min} ${ampm}`){
-            console.log("Alarm Triggered")
-            this.isAlarmTriggered = true
-            this.ringtone.play()
-            this.ringtone.loop = true
-        }
-        }, 1000)
-
-    }
+    currentTimeHandler() {
+        setInterval(() => {
+          let dateTime = new Date();
+          let hour = dateTime.getHours();
+          let min = dateTime.getMinutes();
+          let sec = dateTime.getSeconds();
+          let ampm = "AM";
+      
+          if (hour === 0) {
+            hour = 12;
+            ampm = "AM";
+          } else if (hour === 12) {
+            ampm = "PM";
+          } else if (hour >= 12) {
+            hour = hour - 12;
+            ampm = "PM";
+          }
+      
+          hour = hour < 10 ? "0" + hour : hour;
+          min = min < 10 ? "0" + min : min;
+          sec = sec < 10 ? "0" + sec : sec;
+      
+          this.currentTime = `${hour}:${min}:${sec} ${ampm}`;
+      
+          if (this.alarmTime === `${hour}:${min} ${ampm}`) {
+            console.log("Alarm Triggered!!");
+            this.isAlarmTriggered = true;
+            this.ringtone.play();
+            this.ringtone.loop = true;
+          }
+        }, 1000);
+      }
 
     createHoursOptions(){
         for(let i =1; i<= 12; i++){
